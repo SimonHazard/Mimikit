6aa307f235ca7cc3a4f6d94971b2172c2e613c50
Simon HAZARD <simon.hazard@outlook.fr>
2025-08-03 17:56:46 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Mimikit
+
+It is simply a private website created for fun, based on:
+
+- TanStack Start
+- React
+- TypeScript
+- Tailwindcss + shadcn/ui
+- TanStack Store
+- Supabase
+- Netlify
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		tsConfigPaths(),
+		tanstackStart({ customViteReactPlugin: true }),
+		viteReact(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
